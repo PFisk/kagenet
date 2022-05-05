3498e6f0f2f5252ad47cecda2d1b2b38e2053814
Philip <phfi@itu.dk>
2022-05-05 17:08:21 +0200

    facts page updates

--- components/Cover.js
+++ components/Cover.js
@@ -1,30 +1,56 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
 
+
 const Cover = (props) => {
 
     const data = props.data;
 
+    function dataFormatter(data) {
+        const formattedData = [];
+
+        formattedData.push({
+            rock: data.albumGuesses.filter(e => e.get('guess') === 'rock').length,
+            pop: data.albumGuesses.filter(e => e.get('guess') === 'pop').length,
+            hip_hop: data.albumGuesses.filter(e => e.get('guess') === 'hip hop').length,
+            electronic: data.albumGuesses.filter(e => e.get('guess') === 'electronic').length,
+            jazz: data.albumGuesses.filter(e => e.get('guess') === 'jazz').length,
+            folk_country: data.albumGuesses.filter(e => e.get('guess') === 'folk / country').length,
+            funk_soul: data.albumGuesses.filter(e => e.get('guess') === 'funk / soul').length,
+            classical: data.albumGuesses.filter(e => e.get('guess') === 'classical').length,
+            blues: data.albumGuesses.filter(e => e.get('guess') === 'blues').length,
+            reggae: data.albumGuesses.filter(e => e.get('guess') === 'reggae').length,
+            latin: data.albumGuesses.filter(e => e.get('guess') === 'latin').length,
+            total: data.albumGuesses.length
+        })
+
+        return formattedData;
+    }
+
+    const guesses = dataFormatter(data);
+
+    console.log(guesses);
+
+
     return (
         <div className={styles.factcard}>
             <h2>
-                {data.id}
+                {data.albumID}
             </h2>
             <div className={styles.card_image}>
-            <img
-                className={styles.image}
-                src={data.image_url}
-                alt="album cover"
-              />
-                
+                <img
+                    className={styles.image}
+                    src={data.image}
+                    alt="album cover"
+                />
+
             </div>
             <div className={styles.card_body}>
                 <p>
-                    {data.id}
+                    Rock: {guesses[0].rock}
                 </p>
             </div>
         </div>
-
     );
 
 };
--- pages/factlist.js
+++ pages/factlist.js
@@ -5,12 +5,51 @@ import getAllImages from "../data/imageData.js";
 import Cover from '../components/Cover.js';
 import React, { useEffect } from 'react';
 
+function getAlbumGuesses(albumData, surveyResponses, imageData) {
+
+    const albumGuessData = [];
+
+    albumData.forEach(album => {
+        const albumID = album.get('album_id');
+        const isAlbumReal = album.get('real');
+        const genre = album.get('album_genre');
+        const albumGuesses = surveyResponses.filter(response => response.get('cover_name') === albumID);
+        const albumImage = imageData.filter(image => image.id === albumID)[0].image_url;
+
+        if (!albumGuessData.some(e => e.albumID === albumID)) {
+
+            albumGuessData.push({
+                albumID: albumID,
+                real: isAlbumReal,
+                genre: [genre],
+                image: albumImage,
+                albumGuesses: albumGuesses
+            });
+
+        } else {
+            albumGuessData.map(e => {
+                if (e.albumID === albumID) {
+                    e.genre.push(genre)
+                }
+            })
+
+        }
+
+    })
+
+    console.log(albumGuessData);
+
+    return albumGuessData;
+
+}
+
 export default function Facts() {
 
     const [surveyData, setSurveyData] = React.useState({});
     const [albumData, setAlbumData] = React.useState({});
     const [imageData, setImageData] = React.useState({});
     const [loading, setLoading] = React.useState(true);
+    const [finalCardData, setFinalCardData] = React.useState({});
 
     useEffect(() => {
         async function fetchData() {
@@ -24,7 +63,7 @@ export default function Facts() {
                 setAlbumData(res);
             });
             console.log("Got album data")
-            
+
             await getAllImages().then((res) => {
                 setImageData(res);
                 setLoading(false);
@@ -47,7 +86,7 @@ export default function Facts() {
 
     useEffect(() => {
         if (Object.keys(imageData).length === 0) return
-
+        setFinalCardData(getAlbumGuesses(albumData, surveyData, imageData));
     }, [imageData])
 
 
@@ -57,17 +96,17 @@ export default function Facts() {
 
                 <div className={styles.container}>
                     <div className={styles.title}>
-                        <h1>
+                        <h2>
                             Facts
-                        </h1>
+                        </h2>
                     </div>
                     <div className={styles.main}>
                         <div className={styles.factgrid}>
-                        {Object.keys(imageData).map((key) => {
-                            return (
-                                <Cover key={key} data={imageData[key]} />
-                            )
-                        })}
+                            {Object.keys(finalCardData).map((key) => {
+                                return (
+                                    <Cover key={key} data={finalCardData[key]} />
+                                )
+                            })}
                         </div>
                     </div>
                 </div>
